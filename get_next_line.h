138b995f0acd3336af9ceafd3cbbb09ffd30a492
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-10 17:10:17 +0200

    k

--- get_next_line.h
+++ get_next_line.h
@@ -6,6 +6,15 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:05:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/10 16:05:59 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/10 17:10:08 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+#ifndef BUFF_SIZE
+# define BUFF_SIZE
+int get_next_line(const int fd, char **line);
+
+#endif
