f4566d3cd7e25fd052db764fb4407c3aa2e9e8ae
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-11 17:00:59 +0200

    figuring it out slowly but surely

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,18 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:00:43 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/10 16:00:46 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/11 17:00:14 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int get_next_line(const int fd, char **line)
+{
+	char	buf[BUFF_SIZE + 1];
+
+	if (fd < 0 || !line || read(fd, buf, 0) < 0)
+		return (-1);
+
+	read(fd, buff, BUFF_SIZE);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,17 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:05:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/10 17:10:08 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/11 17:00:18 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
 
-#ifndef BUFF_SIZE
-# define BUFF_SIZE
+#include <unistd.h>
+
+
 int get_next_line(const int fd, char **line);
 
 #endif
