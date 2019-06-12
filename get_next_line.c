0d1946d314f60f8027387d30be98097ba507bde4
Rigardt Engelbrecht <rengelbr@c3r6s2.wethinkcode.co.za>
2019-06-12 10:49:16 +0200

    k

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:00:43 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/06/11 17:00:14 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/06/12 10:48:53 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int get_next_line(const int fd, char **line)
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
 
-	read(fd, buff, BUFF_SIZE);
+	read(fd, buf, BUFF_SIZE);
 }
